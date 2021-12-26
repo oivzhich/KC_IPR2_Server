9e702578d3b1ab88a2f9568e5aeb11e5d3737568
Volha Iuzhychenka <Volha_Iuzhychenka@epam.com>
2021-12-26 22:12:30 +0100

    Search and view all added

--- KC_IPR2_Server/KC_IPR2_Server.cpp
+++ KC_IPR2_Server/KC_IPR2_Server.cpp
@@ -1,77 +1,67 @@
-﻿// KC_IPR2_Server.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#pragma comment(lib, "ws2_32.lib")
+﻿#pragma comment(lib, "ws2_32.lib")
 #include <iostream>
 #include <vector>
-//#include <sys/types.h>
-//
-//#include <clocale>
-//#include <iostream>
-//#include <Windows.h>
-//#include <sys/types.h>
-//#include <string.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <signal.h>
-//#include <winsock2.h>
-//using namespace std;
 
 #include <stdio.h>
 #include <winsock2.h>
+#include <string>
+#include <algorithm>
+#include <format>
 using namespace std;
 
 //Создаем массив структор для хранения информации о списке книг
 struct book_data {
-	char registration_number[40];  // регистрационный номер книги
-	char author[40];  // автор 
-	char name[40]; // название
-	char year_published[40];  // год издания 
-	char publisher[40]; //издательство
-	char quantity_page[40]; //количество страниц
+	string registration_number;  // регистрационный номер книги
+	string author;  // автор 
+	string name; // название
+	int year_published;  // год издания 
+	string publisher; //издательство
+	int quantity_page; //количество страниц
 };
+
+vector<book_data> books_list;
 
 void feel_data(vector<book_data>& books) {
 	book_data book;
 
-	strcpy_s(book.registration_number, "ISBN-10 111");
-	strcpy_s(book.author, "Pushkin");
-	strcpy_s(book.name, "Ruslan i Ludmila");
-	strcpy_s(book.year_published, "2021");
-	strcpy_s(book.publisher, "Belyi dom");
-	strcpy_s(book.quantity_page, "630");
+	book.registration_number = "ISBN-10 111";
+	book.author = "Pushkin";
+	book.name = "Ruslan i Ludmila";
+	book.year_published = 2021;
+	book.publisher = "Belyi dom";
+	book.quantity_page = 630;
 	books.push_back(book);
 
-	strcpy_s(book.registration_number, "ISBN-10 112");
-	strcpy_s(book.author, "Pushkin");
-	strcpy_s(book.name, "Dubrovskiy");
-	strcpy_s(book.year_published, "2020");
-	strcpy_s(book.publisher, "Belyi dom");
-	strcpy_s(book.quantity_page, "201");
+	book.registration_number = "ISBN-10 112";
+	book.author = "Pushkin";
+	book.name = "Dubrovskiy";
+	book.year_published = 2020;
+	book.publisher = "Belyi dom";
+	book.quantity_page = 201;
 	books.push_back(book);
 
-	strcpy_s(book.registration_number, "ISBN-10 113");
-	strcpy_s(book.author, "Esenin");
-	strcpy_s(book.name, "Pismo k gencine");
-	strcpy_s(book.year_published, "2019");
-	strcpy_s(book.publisher, "Skaz");
-	strcpy_s(book.quantity_page, "373");
+	book.registration_number = "ISBN-10 113";
+	book.author = "Esenin";
+	book.name = "Pismo k gencine";
+	book.year_published = 2019;
+	book.publisher = "Skaz";
+	book.quantity_page = 373;
 	books.push_back(book);
 
-	strcpy_s(book.registration_number, "ISBN-10 114");
-	strcpy_s(book.author, "Esenin");
-	strcpy_s(book.name, "Chernyi chelovek");
-	strcpy_s(book.year_published, "2000");
-	strcpy_s(book.publisher, "Skaz");
-	strcpy_s(book.quantity_page, "100");
+	book.registration_number = "ISBN-10 114";
+	book.author = "Esenin";
+	book.name = "Chernyi chelovek";
+	book.year_published = 2000;
+	book.publisher = "Skaz";
+	book.quantity_page = 100;
 	books.push_back(book);
 
-	strcpy_s(book.registration_number, "ISBN-10 115");
-	strcpy_s(book.author, "Bulgakov");
-	strcpy_s(book.name, "Master i Margarita");
-	strcpy_s(book.year_published, "1999");
-	strcpy_s(book.publisher, "Skaz");
-	strcpy_s(book.quantity_page, "500");
+	book.registration_number = "ISBN-10 115";
+	book.author = "Bulgakov";
+	book.name = "Master i Margarita";
+	book.year_published = 1999;
+	book.publisher = "Skaz";
+	book.quantity_page = 500;
 	books.push_back(book);
 }
 
@@ -85,13 +75,84 @@ void print_books(vector<book_data>& books) {
 	}
 }
 
+string get_all_books() {
+	string message;
+	for (book_data book : books_list) {
+		string str = "[" + book.registration_number + "] " + book.author + ": " + book.name + ", " + book.publisher +
+			", " + to_string(book.year_published) + " - " + to_string(book.quantity_page) + " c\n";
+		message += str;
+	}
+	return message;
+}
+
 DWORD WINAPI ThreadFunc(LPVOID client_socket) {
+	char main_menu_choise, // yes
+		switch_menu_choise,	//yes
+		buf[500],
+		result_quantity[30]; //yes
+
+	int num = 0;
+
 	SOCKET s2 = ((SOCKET*)client_socket)[0];
-	char buf[100];
-	recv(s2, buf, sizeof(buf), 0);
-	cout << "Server received command: " << buf << endl;
-	send(s2, buf, 100, 0);
-	closesocket(s2);
+
+	while (true) {
+		memset(&buf[0], 0, sizeof(buf));
+		//Получаем команду от клиента
+		recv(s2, buf, sizeof(buf), 0);
+		main_menu_choise = buf[0];
+		int books_size;
+		vector<book_data> books_filtered;
+		string author_name = "";
+		string result_books_names;
+		string message;
+
+		cout << "Server received command: " << buf << endl;
+
+		switch (main_menu_choise) {
+		case '1':
+			memset(&buf[0], 0, sizeof(buf));
+			//получаем имя автора
+			recv(s2, buf, sizeof(buf), 0);
+			buf[strcspn(buf, "\n")] = 0;
+			author_name = buf;
+
+			cout << "Recieved author name: " << author_name << endl;
+
+			copy_if(books_list.begin(), books_list.end(), back_inserter(books_filtered),
+				[&author_name](auto const& book)
+				{ return book.author == author_name; });
+
+			if (books_filtered.size() == 0) {
+				message = "No author found. Please try to enter different author";
+			}
+			else {
+				for (unsigned i = 0; i < books_filtered.size(); i++) {
+					cout << books_filtered[i].name << endl;
+					message += books_filtered[i].name + "\n";
+				}
+			}
+
+			//отправляем результат поиска
+			send(s2, message.c_str(), message.size(), 0);
+			break;
+		case '2':
+			if (books_list.size() > 0) {
+				message = get_all_books();
+			}
+			else {
+				message = "No author found. Please try to enter different author";
+			}
+			//отправляем результат
+			send(s2, message.c_str(), message.size(), 0);
+			break;
+
+		default:
+			cout << "Coonection will be closed" << endl;
+			closesocket(s2);
+			exit(0);
+			break;
+		}
+	}
 	return 0;
 }
 
@@ -101,8 +162,7 @@ void printConnectionIsReady(int& clientNumber) {
 }
 
 int main() {
-	//vector<book_data> books_list;
-	//feel_data(books_list);
+	feel_data(books_list);
 	//print_books(books_list);
 	//cout << "Size of data: " << size(books_list) << endl;
 
